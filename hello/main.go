56a096e901f69e6191a63c4c87ae48e0a6e27052
moiz-xyz <a.moizniaxi123@gmail.com>
2026-06-17 23:35:15 +0500

    first commit

---
+++ hello/main.go
@@ -0,0 +1,13 @@
+// package main
+// import "fmt"
+
+// func main() {
+//     fmt.Println("Hello, go lang!")
+// }
+
+
+package main
+import "fmt"
+func main(){
+	fmt.Println("hello hand written")
+}
