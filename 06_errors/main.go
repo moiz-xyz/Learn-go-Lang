50e0650bd15feeb2c683925b6f0a85f3c3425caf
moiz-xyz <a.moizniaxi123@gmail.com>
2026-06-24 23:04:00 +0500

    error handling in go

---
+++ 06_errors/main.go
@@ -0,0 +1,50 @@
+// Error handling is a very important concept in Go. Unlike languages that use exceptions
+// (try/catch), Go handles errors by returning them as values.
+package main
+
+import (
+	"fmt"
+)
+
+
+func getUser(id int) (string, error) {
+	if id <= 0 {
+		return "", fmt.Errorf("invalid user id")
+	}
+
+	return "Moiz", nil
+}
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("cannot divide by zero")
+	}
+
+	return a / b, nil
+}
+
+func main() {
+	result, err := divide(10, 0)
+//  nill meaan no error
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Result:", result)
+}
+
+
+func validateAge(age int) error {
+	if age < 18 {
+		return errors.New("age must be 18 or older")
+	}
+	return nil
+}
+
+func main() {
+	err := validateAge(16)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
